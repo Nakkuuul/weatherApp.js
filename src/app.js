06bedb23786b606dc955c8525d9070c1526b5208
sapphirebroking <nakul.thakur@sapphirebroking.com>
2026-04-15 00:53:06 +0530

    feat: health check route

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-  res.send("Server alive");
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running",
+  });
 });
 
 app.use("/api/v1/auth", authRouter);
