456cc42afd1efb270dd5ca0f2043e4b473b903e0
sapphirebroking <nakul.thakur@sapphirebroking.com>
2026-04-15 16:42:42 +0530

    feat: getWeather controller and route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import env from "../config/env.js";
 
+import weatherService from "../services/weather.service.js";
+
 export async function register(req, res) {
   try {
     const { email, password } = req.body;
@@ -104,6 +106,62 @@ export async function login(req, res) {
       },
       token: token,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+}
+
+export async function getWeather(req, res) {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const { cityName } = req.body;
+
+    if (!cityName) {
+      return res.status(400).json({
+        success: false,
+        message: "City name is required",
+      });
+    }
+
+    let decoded;
+    try {
+      decoded = jwt.verify(token, env.JWT_SECRET);
+      req.user = decoded;
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or Expired Token",
+      });
+    }
+
+    const weatherData = await weatherService(cityName);
+
+    if (!weatherData) {
+      return res.status(404).json({
+        success: false,
+        message: "City not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: weatherData,
+    });
+
   } catch (error) {
     return res.status(500).json({
       success: false,
